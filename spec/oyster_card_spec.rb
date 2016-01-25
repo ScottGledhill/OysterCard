2dafcb5951fc2feffeb75fa6b14ad263118d8e78
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-01-25 14:07:47 +0000

    add the balance

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -2,4 +2,12 @@ require_relative '../lib/oyster_card.rb'
 
 describe OysterCard do
 
+subject(:OysterCard) {described_class.new}
+
+  describe 'Balance' do
+    it 'balance starts at 0' do
+      subject.balance
+      expect(subject.balance).to eq 0
+    end
+  end
 end
