3053687903830d53fb05e5b6c3b1c8e485475b92
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-01-25 15:13:24 +0000

    balance exceeded prevented

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,12 +1,13 @@
 
+MAXIMUM_LIMIT = 90
+
 class OysterCard
 attr_reader :balance
-
   def initialize
     @balance = 0
   end
 
   def topup(value)
-    @balance += value
+    (@balance + value) > MAXIMUM_LIMIT ? 'exceeded' : @balance += value
   end
 end
